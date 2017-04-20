cabcc26b85cd1f2fd91d17368242031200dca5da
Jack Ryder <JackRyder@users.noreply.github.com>
2017-04-20 14:15:50 +0100

    Added +play to help messages

--- SPBot/Statics.cs
+++ SPBot/Statics.cs
@@ -11,7 +11,8 @@ namespace SPBot
         public static string HelpMessages()
         {
             string Commands = "";
-            Commands = "!help (which i'm guessing you know) to get these commands." + Environment.NewLine;
+            Commands = "+help (which i'm guessing you know) to get these commands." + Environment.NewLine;
+            Commands += "+play <Youtube URL> to play the requested song." + Environment.NewLine;
             Commands += "And that's all I can do at the moment! ^_^";
             return Commands;
         }
