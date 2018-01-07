e0e84b20e14484f147da1d07a4b39093b664213f
Jack Ryder <me@jackryder.co.uk>
2018-01-07 00:43:48 +0000

    added cryptocurrency checking

---
+++ SPBot.Core/Modules/CryptocurrencyModule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Discord;
+using Discord.Audio;
+using Discord.Commands;
+using System.Net;
+using System.Xml.Linq;
+
+namespace SPBot.Core
+{
+    public class CryptocurrencyModule : ModuleBase
+    {
+        [Command("CheckCoin", RunMode = RunMode.Async)]
+        public async Task CheckCoin(string SearchQuery)
+        {
+            string ApiEndpoint = "https://api.kucoin.com/v1/open/currencies?coins=" + SearchQuery.ToUpper();
+            using (WebClient WC = new WebClient())
+            {
+                string response = await WC.DownloadStringTaskAsync(ApiEndpoint);
+                Newtonsoft.Json.Linq.JToken Token = Newtonsoft.Json.Linq.JToken.Parse(response);
+                if(Token.SelectToken("success").ToObject<bool>())
+                {
+                    string USDValue = Token.SelectToken("data.rates.DRGN.USD").ToString();
+                    string GBPValue = Token.SelectToken("data.rates.DRGN.GBP").ToString();
+                    string MessageStr = "1 " + SearchQuery + " is currently worth $" + USDValue + " or £" + GBPValue;
+                    await Context.Message.Channel.SendMessageAsync(MessageStr);
+                }
+            }
+        }
+    }
+}
