39bb6248c6de58062cd08b96899b22aa490d6430
Jack Ryder <me@jackryder.co.uk>
2018-01-07 00:52:34 +0000

    added error message to crypto

--- SPBot.Core/Modules/CryptocurrencyModule.cs
+++ SPBot.Core/Modules/CryptocurrencyModule.cs
@@ -16,17 +16,22 @@ namespace SPBot.Core
         [Command("CheckCoin", RunMode = RunMode.Async)]
         public async Task CheckCoin(string SearchQuery)
         {
-            string ApiEndpoint = "https://api.kucoin.com/v1/open/currencies?coins=" + SearchQuery.ToUpper();
+            SearchQuery = SearchQuery.ToUpper();
+            string ApiEndpoint = "https://api.kucoin.com/v1/open/currencies?coins=" + SearchQuery;
             using (WebClient WC = new WebClient())
             {
                 string response = await WC.DownloadStringTaskAsync(ApiEndpoint);
                 Newtonsoft.Json.Linq.JToken Token = Newtonsoft.Json.Linq.JToken.Parse(response);
                 if(Token.SelectToken("success").ToObject<bool>())
                 {
-                    string USDValue = Token.SelectToken("data.rates.DRGN.USD").ToString();
-                    string GBPValue = Token.SelectToken("data.rates.DRGN.GBP").ToString();
+                    string USDValue = Token.SelectToken($"data.rates.{SearchQuery}.USD").ToString();
+                    string GBPValue = Token.SelectToken($"data.rates.{SearchQuery}.GBP").ToString();
                     string MessageStr = "1 " + SearchQuery + " is currently worth $" + USDValue + " or £" + GBPValue;
                     await Context.Message.Channel.SendMessageAsync(MessageStr);
+                }
+                else
+                {
+                    await Context.Message.Channel.SendMessageAsync("Cannot find any data for " + SearchQuery + ", did you mistype it?");
                 }
             }
         }
