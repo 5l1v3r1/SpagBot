1ffbc46a4e7d71c39f285cd0af2991e10f8896b0
Jack Ryder <me@jackryder.co.uk>
2018-01-15 13:45:30 +0000

    Updated HelpModule.

--- SPBot.Core/Modules/HelpModule.cs
+++ SPBot.Core/Modules/HelpModule.cs
@@ -1,6 +1,8 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,11 +10,45 @@ namespace SPBot.Core
 {
     public class HelpModule : ModuleBase
     {
+        private CommandService _service;
+
+        public HelpModule(CommandService service)
+        {
+            _service = service;
+        }
+
         [Command("Help", RunMode = RunMode.Async)]
         public async Task Help()
         {
             var DMToUserObject = await Context.User.GetOrCreateDMChannelAsync();
-            await DMToUserObject.SendMessageAsync(Statics.HelpMessages());
+            string prefix = "+";
+            var builder = new EmbedBuilder()
+            {
+                Color = new Color(114, 137, 218),
+                Description = "These are the commands you can use"
+            };
+
+            foreach (var module in _service.Modules)
+            {
+                string description = null;
+                foreach (var cmd in module.Commands)
+                {
+                    var result = await cmd.CheckPreconditionsAsync(Context);
+                    if (result.IsSuccess)
+                        description += $"{prefix}{cmd.Aliases.First()}\n";
+                }
+
+                if (!string.IsNullOrWhiteSpace(description))
+                {
+                    builder.AddField(x =>
+                    {
+                        x.Name = module.Name;
+                        x.Value = description;
+                        x.IsInline = false;
+                    });
+                }
+            }
+            await DMToUserObject.SendMessageAsync("",false, builder.Build());
         }
     }
 }
